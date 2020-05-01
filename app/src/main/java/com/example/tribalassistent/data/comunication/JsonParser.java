4e9f88f3ed7d20193d32266a8b602795fb5af280
Sérgio Araújo <sergiozaraujoz98@gmail.com>
2020-05-01 19:57:08 +0100

    Json parser

---
+++ app/src/main/java/com/example/tribalassistent/data/comunication/JsonParser.java
@@ -0,0 +1,20 @@
+package com.example.tribalassistent.data.comunication;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class JsonParser {
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+
+    public static <T> EventMsg<T> parseEvent(JSONObject jsonObject, Class<T> clazz) {
+        try {
+            return MAPPER.readValue(jsonObject.toString(), MAPPER.getTypeFactory().constructParametricType(EventMsg.class, clazz));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
