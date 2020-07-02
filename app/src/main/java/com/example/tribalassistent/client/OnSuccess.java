5be0a0440e444af9d2f7803b2a1a694540057c91
Sérgio Araújo <sergiozaraujoz98@gmail.com>
2020-07-02 19:45:41 +0200

    Corrected default error handler

--- app/src/main/java/com/example/tribalassistent/client/OnSuccess.java
+++ app/src/main/java/com/example/tribalassistent/client/OnSuccess.java
@@ -1,10 +1,12 @@
 package com.example.tribalassistent.client;
 
+import android.util.Log;
+
 import com.example.tribalassistent.data.model.system.Error;
 
 public abstract class OnSuccess<T> implements AsyncCallback<T> {
     @Override
     public void onFailure(Error error) {
-        System.out.println(error.getMessage());//TODO
+        Log.d("DefaultHandler", error.getMessage());
     }
 }
